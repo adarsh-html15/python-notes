66fe46dfdc9cd95075aed93e7d4da82f22d5f31b
adarsh-html15 <mauryadarsh15@gmail.com>
2026-08-14 15:30:22 +0000

    python

---
+++ Lecture-1/String.py
@@ -0,0 +1,44 @@
+str1 = "this is string."
+# str2 = "this is also string."
+# str3 = """ this is also a string."""
+
+# Scape squence car (\t)
+
+# Concatination
+print("this is one"+" "+"this is two")
+ 
+# to find length of a string len()
+print(len(str1))
+
+# Indexing
+# every str gets a index python, start with 0
+print(str1[0])
+print(str1[1])
+print(str1[2])
+
+# Sclicing
+str = "tHEnAMEiSbOSS"
+print(str[1:4])
+print(str[0:len(str)])
+""" A P P L E 
+   -5-4-3-2-1
+"""
+
+# String function
+''' Answer will cpme in true/false
+    str = "i am a coder"
+    str.endswith("er.")
+    str.captialize
+    str.replace(old, new)
+    str.find(word)
+    str.count("am")
+    '''
+    #Conditional Statement
+    #if-elif-else
+age = 21
+if(age>20):
+    print("can vote")
+elif(age<20): 
+    print("can't vote")
+else:
+    print("also can wait")
---
+++ Lecture-1/one.py
@@ -0,0 +1,32 @@
+#commet
+print("Hey this is my new language","and i am learning it.")
+name = "Adarsh"
+age = 21
+print(name,age)
+print("My age is ," ,age)
+print(type(name))
+print(type(age))
+# datatype
+''' int, string, bol, float, none
+ Keywords { and ,els, in,return, true, while....etc}'''
+
+""" this is a multiline comment"""
+# Opertor
+""" arithmatic(+,*,-,/)
+    Relational/comparison (==,!=)
+    Assignment(=,+=,/+,)
+    logical (<,>,and,or)
+                        """
+ #type conversion(autometically type converting (callsed implicite conversion))
+ #type castion (PRogrammer convert the type manually or forcefully(Explicite conversion))
+ 
+a = int("2")
+b = 4.25
+print(type(a))
+print(type(b))
+
+# take input
+
+test = input("Enter your name: ")
+print("My name is:",test)
+
